fff1e14d22261cf2af0f78ed5c4e0271c3dd82ea
xande03 <104875527+xande03@users.noreply.github.com>
2026-03-25 11:53:08 -0300

    Adicionado frase de boas-vindas no painel de início

---
+++ src/components/PainelInicio.tsx
@@ -0,0 +1,14 @@
+```typescript
+import React from 'react';
+
+const PainelInicio: React.FC = () => {
+  return (
+    <div>
+      <h1>Olá Guerreiros</h1>
+      {/* Outros componentes ou conteúdo do painel de início */}
+    </div>
+  );
+};
+
+export default PainelInicio;
+```
