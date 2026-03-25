ed2c987767ea6c5538ad2034d3c97dbf94ef06ee
xande03 <104875527+xande03@users.noreply.github.com>
2026-03-25 12:12:06 -0300

    Adicionado frase de boas-vindas no painel da sessão de home

---
+++ src/components/HomeSessionPanel.tsx
@@ -0,0 +1,14 @@
+```typescript
+import React from 'react';
+
+const HomeSessionPanel: React.FC = () => {
+  return (
+    <div>
+      <h1>Olá, guerreiros!</h1>
+      {/* Outros componentes ou conteúdo do painel */}
+    </div>
+  );
+};
+
+export default HomeSessionPanel;
+```
